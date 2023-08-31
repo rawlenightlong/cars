9f5315abe772e25d9499285f39937e5cee43bc09
Rawle Becerril <rawlesxm@gmail.com>
2023-08-30 21:45:21 -0400

    hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import { CustomButton } from "."
 const Hero = () => {
 
     const handleScroll = () => {}
-    
+
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
@@ -20,6 +20,13 @@ const Hero = () => {
             containerStyles="bg-primary-blue text-white rounded-full mt-10"
             handleClick={handleScroll}
             />
+        </div>
+        <div className="hero__image-container"> 
+            <div className="hero__image">
+                <Image src ="/hero.png" alt="hero" 
+                fill className="object-contain"/>
+                <div className="hero__image-overlay"/>
+            </div>
         </div>
     </div>
   )
